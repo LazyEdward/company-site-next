876a7a7cf84548855f23983d95394f90db753b13
LazyEdward <yeung_edward@yahoo.com>
2024-07-11 01:31:15 +0900

    change default timeout

--- cypress/e2e/nav.cy.ts
+++ cypress/e2e/nav.cy.ts
@@ -11,13 +11,12 @@ describe('Test TopBar Navigation', () => {
 	describe('Desktop', () => {
 		beforeEach(() => {
 			cy.viewport(1920, 1080)
-			// cy.viewport(350, 700)
 		})
 
 		it('Test TopBar Link exist', () => {
-			cy.get('[data-cy="header-nav-image"]', { timeout: 10000 }).should('be.visible')
-			cy.get('[data-cy="footer-nav-image"]', { timeout: 10000 }).scrollIntoView().should('be.visible')
-			const desktopLinks = cy.get('[data-cy="desktop-links"]', { timeout: 10000 })
+			cy.get('[data-cy="header-nav-image"]').should('be.visible')
+			cy.get('[data-cy="footer-nav-image"]').scrollIntoView().should('be.visible')
+			const desktopLinks = cy.get('[data-cy="desktop-links"]')
 
 			desktopLinks.should('be.visible')
 			desktopLinks.children().should('have.length', 4)
@@ -33,42 +32,42 @@ describe('Test TopBar Navigation', () => {
 		})
 
 		it('Test TopBar Link nav', () => {
-			const desktopLinks = cy.get('[data-cy="desktop-links"]', { timeout: 10000 }).children()
+			const desktopLinks = cy.get('[data-cy="desktop-links"]').children()
 
 			desktopLinks.then(() => {
-				desktopLinks.contains('How to start', { timeout: 10000 }).should('be.visible').click().then(() => {
+				desktopLinks.contains('How to start').should('be.visible').click().then(() => {
 					cy.url().should('include', '/get-started')
 				})
 	
 				cy.wait(500)
 
-				desktopLinks.contains('Services', { timeout: 10000 }).should('be.visible').click().then(() => {
+				desktopLinks.contains('Services').should('be.visible').click().then(() => {
 					cy.url().should('include', '/services')
 				})
 
 				cy.wait(500)
 
-				desktopLinks.contains('Technologies', { timeout: 10000 }).should('be.visible').click().then(() => {
+				desktopLinks.contains('Technologies').should('be.visible').click().then(() => {
 					cy.url().should('include', '/technologies')
 				})
 
 				cy.wait(500)
 
-				desktopLinks.contains('Careers', { timeout: 10000 }).should('be.visible').click().then(() => {
+				desktopLinks.contains('Careers').should('be.visible').click().then(() => {
 					cy.url().should('include', '/careers')
 				})
 
 				cy.wait(500)
 
-				cy.get('[data-cy="header-nav-image"]', { timeout: 10000 }).should('be.visible').click().then(() => {
+				cy.get('[data-cy="header-nav-image"]').should('be.visible').click().then(() => {
 					cy.url().should('include', '/')
 				})
 
-				cy.contains('span', 'Contact Us', { timeout: 10000 }).should('be.visible').click().then(() => {
+				cy.contains('span', 'Contact Us').should('be.visible').click().then(() => {
 					cy.url().should('include', '/contact-us')
 				})
 
-				cy.get('[data-cy="footer-nav-image"]', { timeout: 10000 }).scrollIntoView().should('be.visible').click().then(() => {
+				cy.get('[data-cy="footer-nav-image"]').scrollIntoView().should('be.visible').click().then(() => {
 					cy.url().should('include', '/')
 				})
 			})
@@ -80,20 +79,20 @@ describe('Test TopBar Navigation', () => {
 	
 	describe('Mobile', () => {
 		beforeEach(() => {
-			cy.viewport(350, 700)
+			cy.viewport(412, 893)
 		})
 
 		it('Test TopBar Link exist', () => {
-			cy.get('[data-cy="header-nav-image"]', { timeout: 10000 }).should('be.visible')
-			cy.get('[data-cy="footer-nav-image"]', { timeout: 10000 }).scrollIntoView().should('be.visible')
+			cy.get('[data-cy="header-nav-image"]').should('be.visible')
+			cy.get('[data-cy="footer-nav-image"]').scrollIntoView().should('be.visible')
 
-			cy.contains('span', 'Contact Us', { timeout: 10000 }).should('not.be.visible')
+			cy.contains('span', 'Contact Us').should('not.be.visible')
 			
-			const menu = cy.get('[role="menu"]', { timeout: 10000 })
+			const menu = cy.get('[role="menu"]')
 			menu.should('be.visible')
 
 			menu.click().then(() => {
-				const mobileLinks = cy.get('[data-cy="mobile-links"]', { timeout: 10000 })
+				const mobileLinks = cy.get('[data-cy="mobile-links"]')
 				mobileLinks.should('be.visible')
 	
 				mobileLinks
@@ -108,41 +107,53 @@ describe('Test TopBar Navigation', () => {
 		})
 
 		it('Test TopBar Link nav', () => {
-			cy.get('[role="menu"]', { timeout: 10000 }).should('be.visible').click().then(() => {
-				cy.get('[data-cy="mobile-links"]', { timeout: 10000 }).children().contains('How to start').should('be.visible').click().then(() => {
+			cy.get('[role="menu"]').should('be.visible').click().then(() => {
+				cy.get('[data-cy="mobile-links"]').children().contains('How to start').should('be.visible').click().then(() => {
 					cy.url().should('include', '/get-started')
 				})
 			})
 
-			cy.get('[role="menu"]', { timeout: 10000 }).should('be.visible').click().then(() => {
-				cy.get('[data-cy="mobile-links"]', { timeout: 10000 }).children().contains('Services').should('be.visible').click().then(() => {
+			cy.wait(500)
+
+			cy.get('[role="menu"]').should('be.visible').click().then(() => {
+				cy.get('[data-cy="mobile-links"]').children().contains('Services').should('be.visible').click().then(() => {
 					cy.url().should('include', '/services')
 				})
 			})
 
-			cy.get('[role="menu"]', { timeout: 10000 }).should('be.visible').click().then(() => {
-				cy.get('[data-cy="mobile-links"]', { timeout: 10000 }).children().contains('Technologies').should('be.visible').click().then(() => {
+			cy.wait(500)
+
+			cy.get('[role="menu"]').should('be.visible').click().then(() => {
+				cy.get('[data-cy="mobile-links"]').children().contains('Technologies').should('be.visible').click().then(() => {
 					cy.url().should('include', '/technologies')
 				})
 			})
 
-			cy.get('[role="menu"]', { timeout: 10000 }).should('be.visible').click().then(() => {
-				cy.get('[data-cy="mobile-links"]', { timeout: 10000 }).children().contains('Careers').should('be.visible').click().then(() => {
+			cy.wait(500)
+
+			cy.get('[role="menu"]').should('be.visible').click().then(() => {
+				cy.get('[data-cy="mobile-links"]').children().contains('Careers').should('be.visible').click().then(() => {
 					cy.url().should('include', '/careers')
 				})
 			})
 
-			cy.get('[data-cy="header-nav-image"]', { timeout: 10000 }).should('be.visible').should('be.visible').click().then(() => {
+			cy.wait(500)
+
+			cy.get('[data-cy="header-nav-image"]').should('be.visible').should('be.visible').click().then(() => {
 				cy.url().should('include', '/')
 			})
 
-			cy.get('[role="menu"]', { timeout: 10000 }).should('be.visible').click().then(() => {
-				cy.get('[data-cy="mobile-links"]', { timeout: 10000 }).children().contains('Contact Us').should('be.visible').click().then(() => {
+			cy.wait(500)
+
+			cy.get('[role="menu"]').should('be.visible').click().then(() => {
+				cy.get('[data-cy="mobile-links"]').children().contains('Contact Us').should('be.visible').click().then(() => {
 					cy.url().should('include', '/contact-us')
 				})
 			})
 
-			cy.get('[data-cy="footer-nav-image"]', { timeout: 10000 }).scrollIntoView().should('be.visible').click().then(() => {
+			cy.wait(500)
+
+			cy.get('[data-cy="footer-nav-image"]').scrollIntoView().should('be.visible').click().then(() => {
 				cy.url().should('include', '/')
 			})
 
